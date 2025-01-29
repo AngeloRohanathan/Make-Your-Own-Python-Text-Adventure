2a759b9bb8c6d2d21938299945ca9d73b577cf5b
Angelo Rohanathan <angelorohanathan3@gmail.com>
2025-01-29 18:22:51 +0000

    Completed homework for chapter 4

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Make-Your-Own-Python-Text-Adventure
 This repo is walks through the textbook Make Your Own Python Text Adventure and records the progress that I make learning python.
+
+## Chapter 2
---
+++ chapter_4_tasks/ages.py
@@ -0,0 +1,5 @@
+age = input('What is your age? ')
+if int(age) < 18:
+    print('You are a child.')
+else:
+    print('you are legally an adult, therefore you can vote and in the UK you can drink alcohol')
