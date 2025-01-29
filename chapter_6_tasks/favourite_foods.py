38d5fc712bfed1ab092ae747d7cd95ee82eff713
Angelo Rohanathan <angelorohanathan3@gmail.com>
2025-01-29 19:31:46 +0000

    Completed tasks for chapter 6

---
+++ chapter_6_tasks/favourite_foods.py
@@ -0,0 +1,8 @@
+fave_food_list = []
+first_food = input('Enter your first favourite food: ')
+second_food = input('Enter your second favourite food: ')
+third_food = input('Enter your third favourite food: ')
+fave_food_list.append(first_food)
+fave_food_list.append(second_food)
+fave_food_list.append(third_food)
+print(fave_food_list)
